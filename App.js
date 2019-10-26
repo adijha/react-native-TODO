c6285fd4d1f97523989ce2a4f008edbcf0c00535
Aditya Kumar Jha <adityajha126@gmail.com>
2019-10-26 15:54:04 +0530

    todo complete

--- App.js
+++ App.js
@@ -1,30 +1,39 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 'react-native';
+import { StyleSheet, View, Button, FlatList } from 'react-native';
+
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
-	const [ enteredGoal, setEnteredGoal ] = useState('');
 	const [ courseGoals, setCourseGoals ] = useState([]);
 
-	const goalInputHandler = (enteredText) => {
-		setEnteredGoal(enteredText);
+	const [ isAddMode, setIsAddMode ] = useState(false);
+
+	const addGoalHandler = (goalTitle) => {
+		setCourseGoals((currentGoals) => [ ...currentGoals, { id: Math.random().toString(), value: goalTitle } ]);
+		setIsAddMode(false);
 	};
-	const addGoalHandler = () => {
-		setCourseGoals((currentGoals) => [ ...currentGoals, { id: Math.random().toString(), value: enteredGoal } ]);
+
+	const removeGoalHandler = (goalId) => {
+		setCourseGoals((currentGoals) => {
+			return currentGoals.filter((goal) => goal.id !== goalId);
+		});
 	};
+
+	const cancelGoalAdditionHandler = () => {
+		setIsAddMode(false)
+	}
+	
 
 	return (
 		<View style={styles.screen}>
-			<View style={styles.inputContainer}>
-				<TextInput placeholder="Course Goal" style={styles.input} onChangeText={goalInputHandler} value={enteredGoal} />
-				<Button title="ADD" onPress={addGoalHandler} />
-			</View>
+			<Button title="Add New Goal" onPress={() => setIsAddMode(true)} />
+			<GoalInput visible={isAddMode} onAddGoal={addGoalHandler} onCancel={cancelGoalAdditionHandler} />
 			<FlatList
 				keyExtractor={(item, index) => item.id}
 				data={courseGoals}
 				renderItem={(itemData) => (
-					<View style={styles.listItems}>
-						<Text>{itemData.item.value}</Text>
-					</View>
+					<GoalItem id={itemData.item.id} onDelete={removeGoalHandler} title={itemData.item.value} />
 				)}
 			/>
 		</View>
@@ -34,23 +43,5 @@ export default function App() {
 const styles = StyleSheet.create({
 	screen: {
 		padding: 50
-	},
-	inputContainer: {
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-		alignItems: 'center'
-	},
-	input: {
-		width: '80%',
-		borderColor: 'black',
-		borderWidth: 1,
-		padding: 10
-	},
-	listItems: {
-		padding: 10,
-		marginVertical: 10,
-		backgroundColor: '#ccc',
-		borderColor: 'black',
-		borderWidth: 1
 	}
 });
